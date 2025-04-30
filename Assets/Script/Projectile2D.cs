1e162df51f3dd313193033698d67037a75beb484
sho-ikeda1 <sho.iked@bumail.net>
2025-05-01 03:14:06 +0700

    add ตัวแปล

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField] Transform shootPoint;
+    [SerializeField] GameObject target; //target sprite
+    [SerializeField] Rigidbody2D bulletPrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
